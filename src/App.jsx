ff5dd6e9bc31b07c1764f159c4905a363a84cfc8
maslog <ronaldmaslog5@gmail.com>
2024-03-31 16:32:11 +0800

    initial commit March 31, 2024

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className=" m-10 h-60 text-center text-5xl font-bold uppercase  bg-amber-300 ">Loader</h1>
+    </div>
+  );
+}
+
+export default App;
